d9796753b6e9d5e1f99847bfdf4466325ef1056f
slimkaki <rafaalmada98@gmail.com>
2017-11-20 17:05:18 -0200

    adicionando interface de pygame para desenho

--- escrita.py
+++ escrita.py
@@ -2,15 +2,20 @@ import pygame,sys,time
 
 pygame.init()
 
-screen=pygame.display.set_mode((1200,720),0,32)
+screen=pygame.display.set_mode((280,280),0,32)
 pygame.display.set_caption('Cluster K-means')
-#myfont=pygame.font.Font("Helvetica.ttf", 40)
+myfont=pygame.font.Font("VeraMoBd.ttf", 25)
 mouse = pygame.mouse
 canvas = screen.copy()
 
 #                     R    G    B
 BLACK = pygame.Color( 0 ,  0 ,  0 )
 WHITE = pygame.Color(255, 255, 255)
+
+apaga=myfont.render("Apagar",1,(255,255,255))
+envia=myfont.render("Enviar",1,(255,255,255))
+
+rect=pygame.Rect(25,25,100,50)
 
 
 
@@ -22,6 +27,8 @@ while(True):
 		if event.type == pygame.KEYDOWN:
 			if event.key==K_ESCAPE:
 				pygame.quit()
+			if event.key==K_RETURN:
+				pygame.image.save(screen)
 		elif left_pressed:
 			pygame.draw.circle(canvas, BLACK, (pygame.mouse.get_pos()),5)
 
@@ -29,4 +36,9 @@ while(True):
 	screen.blit(canvas, (0, 0))
 	pygame.mouse.get_pos()
 	pygame.draw.circle(screen, BLACK, (pygame.mouse.get_pos()), 5)
+
+	#pygame.draw.rect(screen, (0,0,0),(50,50,120,60))
+	#screen.blit(apaga, (62,60))
+	#pygame.draw.rect(screen, (0,0,0),(950,50,1000,60))
+	#screen.blit(envia, (950,50))
 	pygame.display.update()
